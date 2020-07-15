24ef0f5c170be0c5b236055d60a632d5b2e8774e
Michael <mschneider247@gmail.com>
2020-07-14 18:02:12 -0600

    Clean up white margins and remove more unused icons

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,7 @@ h1 {
   font-weight: bold;
 }
 h2 {
+  margin-left: 20px;
   font-size: 2em;
   color:#006643;
   height: 15px;
@@ -98,7 +99,7 @@ h3 {
   color:#006643;
 }
 footer {
-  margin: 1%;
+  margin-top: 2%;
   padding: 1%;
   background-color:#006643;
   color: #fff;
